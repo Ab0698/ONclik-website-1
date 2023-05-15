f36093b96554e5585f0c7fb02820ad394d170da9
Ab0698 <aboubakar0698@gmail.com>
2023-05-15 12:58:39 +0200

    initialisation des fichiers

---
+++ App/Views/users/connexion.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>How to Design Login & Registration Form Transition</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../../../assets/css/style2.css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,800&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="cont">
+    <div class="form sign-in">
+      <h2>Connexion</h2>
+      <label>
+        <span>email</span>
+        <input type="email" name="email">
+      </label>
+      <label>
+        <span>mot de passe</span>
+        <input type="password" name="password">
+      </label>
+      <button class="submit" type="button">S' inscrire</button>
+      <p class="forgot-pass">Mot de passe oublie ?</p>
+
+      <div class="social-media">
+        <ul>
+          <li><img src="../../../assets/image de la grille/images/facebook.png"></li>
+          <li><img src="../../../assets/image de la grille/images/twitter.png"></li>
+          <li><img src="../../../assets/image de la grille/images/linkedin.png"></li>
+          <li><img src="../../../assets/image de la grille/images/instagram.png"></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="sub-cont">
+      <div class="img">
+        <div class="img-text m-up">
+          <h2>Nouveau ici?</h2>
+          <p>Inscrivez-vous et découvrez une grande quantité de nouvelles opportunités!</p>
+        </div>
+        <div class="img-text m-in">
+          <h2>One of us?</h2>
+          <p>Si vous avez déjà un compte, connectez-vous. Vous nous avez manqué !</p>
+        </div>
+        <div class="img-btn">
+          <span class="m-up">S'inscrire</span>
+          <span class="m-in">S'identifier</span>
+        </div>
+      </div>
+      <div class="form sign-up">
+        <h2>Sign Up</h2>
+        <label>
+          <span>Name</span>
+          <input type="text">
+        </label>
+        <label>
+          <span>Email</span>
+          <input type="email">
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="password">
+        </label>
+        <label>
+          <span>Confirm Password</span>
+          <input type="password">
+        </label>
+        <button type="button" class="submit">Sign Up Now</button>
+      </div>
+    </div>
+  </div>
+<script type="text/javascript" src="../../../assets/js/script.js"></script>
+</body>
+</html>
